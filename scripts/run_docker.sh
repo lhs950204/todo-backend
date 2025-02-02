c299df9f273f8ea5a5ef30c0470e36946f6172df
lhs950204 <lhs950204@naver.com>
2025-02-02 21:16:29 +0900

    .

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,8 +1,12 @@
+docker pull lhs950204/todo
+docker rm -f todo
+
 docker run -itd\
  --name todo\
  -p 8000:8000\
  -e SECRET_KEY=09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7\
- -e DB_URI=sqlite://db.db\
+ -e DB_URI=sqlite:///./db/db.db\
  -e MEDIA_ROOT=/media\
- -v $(pwd)/media:/media\
+ -v $(pwd)/s3-buckets/media:/media\
+ -v $(pwd)/db:/db\
  lhs950204/todo
