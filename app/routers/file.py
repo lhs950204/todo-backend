2a3781d88f3108b120b80772e084d3d9eb271a4c
lhs950204 <lhs950204@naver.com>
2025-02-02 21:26:50 +0900

    .

--- app/routers/file.py
+++ app/routers/file.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, UploadFile
 
+from app.depends.user import UserIDDepends
 from app.models.file import File
 
 router = APIRouter(prefix="/files")
 
 
 @router.post("")
-async def upload_file(file: UploadFile, user_id: int):
+async def upload_file(file: UploadFile, user_id: UserIDDepends):
     file_obj = await File.create_from_upload(file, user_id=user_id)
     return {"url": file_obj.get_media_url()}
