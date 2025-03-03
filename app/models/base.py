fa44d339af29b044964c016f868c607efa720741
lhs950204 <lhs950204@naver.com>
2025-03-03 16:21:26 +0900

    버그수정

--- app/models/base.py
+++ app/models/base.py
@@ -7,13 +7,13 @@ from sqlmodel import Field, SQLModel
 class ModelBase(SQLModel):
     id: int = Field(default=None, primary_key=True)
     created_at: datetime = Field(
-        default=datetime.now(timezone.utc),
+        default_factory=lambda: datetime.now(timezone.utc),
         sa_column_kwargs={
             "server_default": func.now(),
         },
     )
     updated_at: datetime = Field(
-        default=datetime.now(),
+        default_factory=lambda: datetime.now(timezone.utc),
         sa_column_kwargs={
             "onupdate": lambda: datetime.now(timezone.utc),
         },
