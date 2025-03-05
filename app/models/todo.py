314629b60e3d9a0eeda54a9f34df4770027b07ac
lhs950204 <lhs950204@naver.com>
2025-03-05 23:57:11 +0900

    수정

--- app/models/todo.py
+++ app/models/todo.py
@@ -13,8 +13,8 @@ if TYPE_CHECKING:
 class Todo(ModelBase, table=True):
     title: str
     done: bool = Field(default=False)
-    link_url: str
-    file_url: str
+    link_url: str | None = None
+    file_url: str | None = None
 
     user_id: int = Field(foreign_key="user.id", nullable=False)
     user: "User" = Relationship(back_populates="todos")
