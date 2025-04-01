e142fa543097731d4fbc35f750f22a6359621df5
lhs950204 <lhs950204@naver.com>
2025-04-01 16:47:42 +0900

    수정

--- app/models/todo.py
+++ app/models/todo.py
@@ -22,4 +22,5 @@ class Todo(ModelBase, table=True):
     goal_id: int = Field(foreign_key="goal.id", nullable=False)
     goal: "Goal" = Relationship(back_populates="todos")
 
+    note_id: int = Field(foreign_key="note.id", nullable=True)
     note: "Note" = Relationship(back_populates="todo")
